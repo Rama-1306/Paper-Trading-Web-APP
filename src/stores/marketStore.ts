3c46f0f32b3a7851e4838767cd74327fb5fdcca0
Rama-1306 <ramarao1807@gmail.com>
2026-03-25 11:11:53 +0530

    Update marketStore.ts

--- src/stores/marketStore.ts
+++ src/stores/marketStore.ts
@@ -143,7 +143,6 @@ export const useMarketStore = create<MarketState>((set, get) => ({
   auth: { token },
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
-  transports: ['polling', 'websocket'],
 });
     
     set({ socket: newSocket });
